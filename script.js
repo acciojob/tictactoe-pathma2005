e39cb48128fd402620ba2667a1afdde0bd28d978
Pathmasri E <pathmasri1372005@gmail.com>
2025-10-25 21:29:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ let board = ["", "", "", "", "", "", "", "", ""];
 // ❌ Wrong: 'cell' was used directly — it doesn’t exist yet
 // ✅ Correct: use submitBtn to start the game
 submitBtn.addEventListener("click", () => {
-  player1 = document.getElementById("player-1").value.trim();
-  player2 = document.getElementById("player-2").value.trim();
+ player1 = document.getElementById("player1").value.trim();
+player2 = document.getElementById("player2").value.trim();
 
   if (player1 === "" || player2 === "") {
     alert("Please enter both player names");
